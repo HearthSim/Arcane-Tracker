177224f51df46a3510d43886d50906c2e92c65c1
Martin Bonnin <martin@mbonnin.net>
2016-11-02 21:48:12 -0700

    added a text on cards

---
+++ app/src/main/java/net/mbonnin/arcanetracker/AspectRatioImageView.java
@@ -0,0 +1,35 @@
+package net.mbonnin.arcanetracker;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.FrameLayout;
+import android.widget.ImageView;
+
+/**
+ * Created by martin on 11/3/16.
+ */
+
+public class AspectRatioImageView extends ImageView {
+    private float mAspect;
+
+    public AspectRatioImageView(Context context) {
+        super(context);
+    }
+
+    public AspectRatioImageView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public void setAspectRatio(float aspect) {
+        mAspect = aspect;
+
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int h = (int) (getMeasuredWidth() * mAspect);
+        setMeasuredDimension(getMeasuredWidth(), h);
+    }
+}
