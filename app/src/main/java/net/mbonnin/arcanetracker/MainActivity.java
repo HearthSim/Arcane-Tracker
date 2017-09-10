c04851efce02609b715ef2474020c31e5487e562
Martin Bonnin <martin@mbonnin.net>
2017-09-10 18:08:04 +0200

    Revert "added oss licenses"
    This reverts commit 18c49f40dcb1a28781e6bbcbc6f9613a0d68a1d7.

--- app/src/main/java/net/mbonnin/arcanetracker/MainActivity.java
+++ app/src/main/java/net/mbonnin/arcanetracker/MainActivity.java
@@ -43,7 +43,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
         Utils.logWithDate("MainActivity.onCreate");
