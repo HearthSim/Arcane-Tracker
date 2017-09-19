6902edb41a186dca37f3a3ec078d5d0f0725d487
Martin Bonnin <martin@mbonnin.net>
2017-09-20 00:14:40 +0200

    use canDrawOverlays everywhere

--- app/src/main/java/net/mbonnin/arcanetracker/MainActivity.java
+++ app/src/main/java/net/mbonnin/arcanetracker/MainActivity.java
@@ -87,7 +87,7 @@ public class MainActivity extends AppCompatActivity {
                 && checkCallingOrSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED;
 
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-            has &= android.provider.Settings.canDrawOverlays(this);
+            has &= canReallyDrawOverlays();
         }
 
         return has;
@@ -165,7 +165,7 @@ public class MainActivity extends AppCompatActivity {
                     || checkCallingOrSelfPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
                 requestPermissions(new String[]{Manifest.permission.READ_EXTERNAL_STORAGE, Manifest.permission.WRITE_EXTERNAL_STORAGE}, REQUEST_CODE_PERMISSIONS);
                 return;
-            } else if (!android.provider.Settings.canDrawOverlays(this)) {
+            } else if (!canReallyDrawOverlays()) {
                 try {
                     Intent intent2 = new Intent(android.provider.Settings.ACTION_MANAGE_OVERLAY_PERMISSION, Uri.parse("package:" + getPackageName()));
                     startActivityForResult(intent2, REQUEST_CODE_GET_OVERLAY_PERMISSIONS);
