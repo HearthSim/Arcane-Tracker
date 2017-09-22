444820b4a365b83bb194e40c8a147f062ae40bd7
Martin Bonnin <martin@mbonnin.net>
2017-09-22 21:26:57 +0200

    evolve the backup script

--- scripts/backup_assets.sh
+++ scripts/backup_assets.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-tar -cvzf arcane_tracker_extra.tar.gz app/keystore.* app/google-services.json app/src/main/assets/bars/ app/src/main/assets/cards app/src/main/assets/fonts/ app/src/test/resources/ranks/
+cp --parents -rf app/keystore.* app/google-services.json app/src/main/assets/bars/ app/src/main/assets/cards app/src/main/assets/fonts/ detector/src/androidTest/resources/models/ ../arcane_data
