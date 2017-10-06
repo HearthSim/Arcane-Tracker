9e19f670956458203dcce38f1d51c8e69bae82f4
Martin Bonnin <martin@mbonnin.net>
2017-10-06 16:56:21 +0200

    move fonts to the res folder

--- app/src/main/java/net/mbonnin/arcanetracker/Typefaces.java
+++ app/src/main/java/net/mbonnin/arcanetracker/Typefaces.java
@@ -1,6 +1,7 @@
 package net.mbonnin.arcanetracker;
 
 import android.graphics.Typeface;
+import android.support.v4.content.res.ResourcesCompat;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -8,32 +9,11 @@ import java.util.Map;
 import timber.log.Timber;
 
 public class Typefaces {
-    private static Map<String, Typeface> sMap = new HashMap();
-
     public static Typeface belwe() {
-        return get("fonts/belwe-bold.ttf");
+        return ResourcesCompat.getFont(ArcaneTrackerApplication.getContext(), R.font.belwe_bold);
     }
 
     public static Typeface franklin() {
-        return get("fonts/franklin-gothic.ttf");
-    }
-
-    public static Typeface get(String assetName) {
-        Typeface typeface = null;
-
-        if (sMap.get(assetName) != null) {
-            return sMap.get(assetName);
-        }
-        try {
-            typeface = Typeface.createFromAsset(ArcaneTrackerApplication.getContext().getAssets(), assetName);
-        } catch (Exception e) {
-            Timber.e(e);
-        }
-
-        if (typeface == null) {
-            typeface = Typeface.DEFAULT;
-        }
-        sMap.put(assetName, typeface);
-        return typeface;
+        return ResourcesCompat.getFont(ArcaneTrackerApplication.getContext(), R.font.franklin_gothic);
     }
 }
