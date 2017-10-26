69629ecb546da043d8865ec4c0cfd6da0de066df
Martin Bonnin <martin@mbonnin.net>
2017-10-27 01:39:33 +0200

    allow to scale the rects

--- detector/src/main/java/net/mbonnin/arcanetracker/detector/RRect.kt
+++ detector/src/main/java/net/mbonnin/arcanetracker/detector/RRect.kt
@@ -1,6 +1,7 @@
 package net.mbonnin.arcanetracker.detector
 
-open class RRect(val x: Double, val y: Double, val w: Double, val h: Double) {
+class RRect(val x: Double, val y: Double, val w: Double, val h: Double) {
     fun scale(sx: Double, sy: Double): RRect = RRect(sx * x, sy * y, sx * w, sy * h)
+    fun translate(tx: Double, ty: Double): RRect = RRect(tx + x, ty + y, w, h)
 }
 
--- detector/src/main/java/net/mbonnin/arcanetracker/detector/RRectFactory.kt
+++ detector/src/main/java/net/mbonnin/arcanetracker/detector/RRectFactory.kt
@@ -25,24 +25,19 @@ class RRectFactory(val screenWidth: Int, val screenHeight: Int, context: Context
     private fun isNexus9Like() = isTablet && screenWidth == 2048 && screenHeight == 1536
     private fun isPixelLike() = !isTablet && screenWidth == 1920 && screenHeight == 1080
 
-    val ARENA_RECTS: Array<RRect>
-        get() {
-            return when {
-                isPixelLike() -> ARENA_RECTS_PIXEL
-                else ->
-                    arrayOf(
-                            scaleRect(RRect(344.138, 1080.0 - 642.198 - 187.951, 185.956, 187.951)),
-                            scaleRect(RRect(854.205, 1080.0 - 642.198 - 187.951, 185.956, 187.951)),
-                            scaleRect(RRect(1379.876, 1080.0 - 642.198 - 187.951, 185.956, 187.951))
-                    )
-            }
+    val ARENA_RECTS by lazy {
+        when {
+            isPixelLike() -> ARENA_RECTS_PIXEL
+            else -> ARENA_RECTS_PIXEL.map(this::scalePhone).toTypedArray()
         }
+    }
 
     val FORMAT by lazy {
         when {
             isNexus9Like() -> FORMAT_NEXUS9
             isPixelLike() -> FORMAT_PIXEL
-            else -> scaleRect(FORMAT_PIXEL)
+            isTablet -> scaleTablet(FORMAT_PIXEL)
+            else -> scalePhone(FORMAT_PIXEL)
         }
     }
 
@@ -50,38 +45,60 @@ class RRectFactory(val screenWidth: Int, val screenHeight: Int, context: Context
         when {
             isNexus9Like() -> RANK_NEXUS9
             isPixelLike() -> RANK_PIXEL
-            else -> scaleRect(RANK_PIXEL)
+            isTablet -> scaleTablet(RANK_NEXUS9)
+            else -> scalePhone(RANK_PIXEL)
         }
     }
 
-    val MODE: RRect
-        get() {
-            return when {
-                isNexus9Like() -> MODE_NEXUS9
-                isPixelLike() -> MODE_PIXEL
-                else -> scaleRect(MODE_PIXEL)
-            }
+    val MODE by lazy {
+        when {
+            isNexus9Like() -> MODE_NEXUS9
+            isPixelLike() -> MODE_PIXEL
+            isTablet -> scaleTablet(MODE_NEXUS9)
+            else -> scalePhone(MODE_PIXEL)
+        }
+    }
+
+    private fun scalePhone(rRect: RRect): RRect {
+        return scale(rRect, 1920.0, 1080.0)
+    }
+
+    fun scaleTablet(rRect: RRect): RRect {
+        return scale(rRect, 2048.0, 1536.0)
+    }
+
+    fun scale(rRect: RRect, refWidth: Double, refHeight: Double): RRect {
+        var scaledWidth: Double
+        var scaledHeight: Double
+
+        if (screenWidth/screenHeight > refWidth/refHeight) {
+            scaledHeight = screenHeight.toDouble()
+            scaledWidth = refWidth * scaledHeight/refHeight
+        } else {
+            scaledWidth = screenWidth.toDouble()
+            scaledHeight = scaledWidth * refHeight / refWidth
         }
 
-    private fun scaleRect(rRect: RRect): RRect {
-        return rRect.scale(screenWidth.toDouble() / 1920.0, screenHeight.toDouble() / 1080.0)
+        val r1 = rRect.scale(scaledWidth/refWidth, scaledHeight/refHeight);
+
+        return r1.translate((screenWidth - scaledWidth) / 2, (screenHeight - scaledHeight)/2)
     }
+
 
     companion object {
-        val FORMAT_NEXUS9 = RRect(1609.0, 39.0, 96.0, 73.0)
         val FORMAT_PIXEL = RRect(1754.0, 32.0, 138.0, 98.0)
+        val FORMAT_NEXUS9 = RRect(1609.0, 39.0, 96.0, 73.0)
 
-        val RANK_NEXUS9 = RRect(1730.0, 201.0, 110.0, 46.0)
         val RANK_PIXEL = RRect(820.0, 424.0, 230.0, 102.0)
+        val RANK_NEXUS9 = RRect(1730.0, 201.0, 110.0, 46.0)
 
-        val MODE_NEXUS9 = RRect(1432.0, 400.0, 160.0, 34.0)
         val MODE_PIXEL = RRect(1270.0, 256.0, 140.0, 32.0)
+        val MODE_NEXUS9 = RRect(1432.0, 400.0, 160.0, 34.0)
 
         val ARENA_RECTS_PIXEL = arrayOf(
                 RRect(344.138, 1080.0 - 642.198 - 187.951, 185.956, 187.951),
                 RRect(854.205, 1080.0 - 642.198 - 187.951, 185.956, 187.951),
                 RRect(1379.876, 1080.0 - 642.198 - 187.951, 185.956, 187.951)
         )
-
     }
 }
