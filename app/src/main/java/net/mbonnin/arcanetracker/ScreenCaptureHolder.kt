4a587bee730f677b61181facadd88e4cdd064761
Martin Bonnin <martin@mbonnin.net>
2017-10-27 21:47:33 +0200

    enable arena tracking in debug mode

--- app/src/main/java/net/mbonnin/arcanetracker/ScreenCaptureHolder.kt
+++ app/src/main/java/net/mbonnin/arcanetracker/ScreenCaptureHolder.kt
@@ -100,7 +100,7 @@ object ScreenCaptureHolder {
                 && ArenaParser.DRAFT_MODE_DRAFTING == ArenaParser.get().draftMode
                 && index >= 0
                 && index < Card.Companion.CLASS_INDEX_NEUTRAL
-                && false
+                && Utils.isAppDebuggable
     }
 
     fun start() {
