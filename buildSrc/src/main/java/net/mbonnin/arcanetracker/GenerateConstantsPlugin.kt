679143bbf7208037c124045034f3eed2b1852a91
Martin Bonnin <martin@mbonnin.net>
2017-11-24 21:15:14 +0100

    added a gradle task to update cards

--- buildSrc/src/main/java/net/mbonnin/arcanetracker/GenerateConstantsPlugin.kt
+++ buildSrc/src/main/java/net/mbonnin/arcanetracker/GenerateConstantsPlugin.kt
@@ -1,5 +1,6 @@
-package net.mbonnin.hsmodel
+package net.mbonnin.arcanetracker
 
+import net.mbonnin.hsmodel.GenerateConstantsTask
 import org.gradle.api.Plugin
 import org.gradle.api.Project
 import org.gradle.api.plugins.JavaPluginConvention
---
+++ buildSrc/src/main/java/net/mbonnin/arcanetracker/UpdateCardsJson.kt
@@ -0,0 +1,11 @@
+package net.mbonnin.arcanetracker
+
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+
+open class UpdateCardsJson: Plugin<Project> {
+    override fun apply(p0: Project?) {
+        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+    }
+
+}
