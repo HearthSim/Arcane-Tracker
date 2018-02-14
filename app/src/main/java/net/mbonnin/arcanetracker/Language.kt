f6e6a1b45b6c36268d512048245b67fb7b34a3aa
Martin Bonnin <martin@mbonnin.net>
2018-02-14 21:32:09 +0100

    also add card translations

--- app/src/main/java/net/mbonnin/arcanetracker/Language.kt
+++ app/src/main/java/net/mbonnin/arcanetracker/Language.kt
@@ -5,56 +5,56 @@ import java.util.*
 class Language(var friendlyName: String, var jsonName: String, var key: String) {
     companion object {
 
-        var allLanguages = ArrayList<Language>()
+        var allLanguages = mutableListOf(
+                Language("English", "enUS", "en"),
+                Language("Français", "frFR", "fr"),
+                Language("Español", "esES", "es"),
+                Language("Italiano", "itIT", "it"),
+                Language("Português", "ptBR", "pt"),
+                Language("Pусский", "ruRU", "ru"),
+                Language("한국의", "koKR", "ko"),
+                Language("中國", "zhTW", "zh"))
 
-        init {
-            allLanguages.add(Language("English", "enUS", "en"))
-            allLanguages.add(Language("Español", "esES", "es"))
-            allLanguages.add(Language("Français", "frFR", "fr"))
-            allLanguages.add(Language("Português", "ptBR", "pt"))
-            allLanguages.add(Language("Pусский", "ruRU", "ru"))
-            allLanguages.add(Language("한국의", "koKR", "ko"))
-            allLanguages.add(Language("中國", "zhTW", "zh"))
-        }
+    val currentLanguage: Language
+        get() {
+            var l: String? = Settings.get(Settings.LANGUAGE, null)
 
-        val currentLanguage: Language
-            get() {
-                var l: String? = Settings.get(Settings.LANGUAGE, null)
+            if (l == null) {
+                val locale = Locale.getDefault().language.toLowerCase()
 
-                if (l == null) {
-                    val locale = Locale.getDefault().language.toLowerCase()
-
-                    if (locale.contains("fr")) {
-                        l = "fr"
-                    } else if (locale.contains("ru")) {
-                        l = "ru"
-                    } else if (locale.contains("pt")) {
-                        l = "pt"
-                    } else if (locale.contains("ko")) {
-                        l = "ko"
-                    } else if (locale.contains("zh")) {
-                        l = "zh"
-                    } else if (locale.contains("es")) {
-                        l = "es"
-                    } else {
-                        l = "en"
-                    }
+                if (locale.contains("fr")) {
+                    l = "fr"
+                } else if (locale.contains("ru")) {
+                    l = "ru"
+                } else if (locale.contains("pt")) {
+                    l = "pt"
+                } else if (locale.contains("ko")) {
+                    l = "ko"
+                } else if (locale.contains("zh")) {
+                    l = "zh"
+                } else if (locale.contains("es")) {
+                    l = "es"
+                } else if (locale.contains("it")) {
+                    l = "it"
+                } else {
+                    l = "en"
                 }
-
-                var language: Language? = null
-                for (language2 in allLanguages) {
-                    if (language2.key == l) {
-                        language = language2
-                        break
-                    }
-                }
-
-                if (language == null) {
-                    language = allLanguages[0]
-                }
-
-                return language
             }
-    }
+
+            var language: Language? = null
+            for (language2 in allLanguages) {
+                if (language2.key == l) {
+                    language = language2
+                    break
+                }
+            }
+
+            if (language == null) {
+                language = allLanguages[0]
+            }
+
+            return language
+        }
+}
 
 }
