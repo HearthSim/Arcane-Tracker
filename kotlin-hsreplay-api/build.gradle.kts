8ed229f6390e992054c7e94369d5f9e57e261bb6
Martin Bonnin <martin@mbonnin.net>
2019-07-21 00:02:10 +0200

    kotlin-hsreplay-api is build.gradle.kts now

---
+++ kotlin-hsreplay-api/build.gradle.kts
@@ -0,0 +1,56 @@
+import org.jetbrains.kotlin.gradle.plugin.KotlinPlatformType
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+plugins {
+    kotlin("multiplatform")
+    id("kotlinx-serialization")
+
+}
+
+kotlin {
+    jvm()
+
+    sourceSets {
+        val commonMain by getting {
+            dependencies {
+                implementation(Libs.stdlibCommon)
+                implementation(Libs.corountinesCommon)
+                implementation(Libs.serializationRuntimeCommon)
+
+                api(Libs.ktorClientCore)
+                api(project(":kotlin-console"))
+                api(project(":kotlin-analytics"))
+
+                implementation(Libs.ktorClientJson)
+                implementation(Libs.ktorClientSerialization)
+                implementation(Libs.ktorClientEncoding)
+            }
+        }
+        jvm().compilations["main"].defaultSourceSet {
+            dependencies {
+                implementation(Libs.stdlibJdk8)
+
+                implementation(Libs.coroutines)
+                implementation(Libs.serializationRuntime)
+                implementation(Libs.ktorClientSerializationJvm)
+
+                api(Libs.ktorClientCoreJvm)
+                implementation(Libs.ktorClientJsonJvm)
+                implementation(Libs.ktorClientOkhttp)
+                implementation(Libs.ktorClientEncodingJvm)
+            }
+        }
+        jvm().compilations["test"].defaultSourceSet {
+            dependencies {
+                implementation(Libs.okhttpLogging)
+                implementation(Libs.coroutines)
+                implementation(Libs.kotlinTestJvm)
+                implementation(Libs.kotlinTestAnnotationJvm)
+            }
+        }
+    }
+}
+
+tasks.withType<KotlinCompile> {
+    kotlinOptions.freeCompilerArgs += "-Xallow-result-return-type"
+}
