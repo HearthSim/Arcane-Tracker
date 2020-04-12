fb15d263d1230d0a64ad5fe1a2c8e6dbc3829174
Martin Bonnin <martin@mbonnin.net>
2020-04-12 20:26:51 +0200

    also compile the debug apk

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,9 +1,10 @@
 #!/usr/bin/env bash
 
-./gradlew :app:assembleRelease jvmTest linkDebugFrameworkMacosX64 linkReleaseFrameworkMacosX64 -Dorg.gradle.jvmargs=-Xmx2g
+./gradlew :app:assembleDebug :app:assembleRelease jvmTest linkDebugFrameworkMacosX64 linkReleaseFrameworkMacosX64 -Dorg.gradle.jvmargs=-Xmx2g
 
 curl -s "https://get.sdkman.io" | bash > /dev/null
 source "$HOME/.sdkman/bin/sdkman-init.sh"
+sdk install kotlin
 sdk install kscript
 
 ./scripts/uploadRelease.kts
