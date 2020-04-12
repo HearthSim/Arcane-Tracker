5533dbb9f37b95b633f05b148777b82317fa9399
Martin Bonnin <martin@mbonnin.net>
2020-04-12 21:38:41 +0200

    try to install a more recent NDK

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -2,6 +2,9 @@
 
 set -x
 
+export PATH="$ANDROID_HOME"/tools/bin:$PATH
+sdkmanager --install 'ndk;21.0.6113669'  >/dev/null
+
 ./gradlew :app:assembleDebug :app:assembleRelease jvmTest linkDebugFrameworkMacosX64 linkReleaseFrameworkMacosX64 -Dorg.gradle.jvmargs=-Xmx2g
 
 curl -s "https://get.sdkman.io" | bash > /dev/null
