bb0c079e29867a01b5c633d181e7bf1a68336261
Martin Bonnin <martin@mbonnin.net>
2020-04-12 21:32:47 +0200

    fail on error

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+set -x
+
 ./gradlew :app:assembleDebug :app:assembleRelease jvmTest linkDebugFrameworkMacosX64 linkReleaseFrameworkMacosX64 -Dorg.gradle.jvmargs=-Xmx2g
 
 curl -s "https://get.sdkman.io" | bash > /dev/null
