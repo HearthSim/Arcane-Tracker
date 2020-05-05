4ca54b8478b40299c6b2e324aaa30b2bcb7a7ede
Martin Bonnin <martin@mbonnin.net>
2020-05-06 01:32:19 +0200

    also release dSYM for release builds

--- scripts/uploadRelease.kts
+++ scripts/uploadRelease.kts
@@ -1,5 +1,4 @@
 #!/usr/bin/env kscript
-@file:MavenRepository("mavenLocal", "file://Users/m.bonnin/.m2/repository/")
 @file:MavenRepository("jcenter", "https://jcenter.bintray.com")
 @file:MavenRepository("gradle", "https://repo.gradle.org/gradle/libs-releases-local/")
 @file:DependsOn("com.dailymotion.kinta:kinta-lib:0.1.9")
@@ -22,27 +21,28 @@ fun zip(dir: String, zipName: String): File {
 println("Signing APK...")
 
 AndroidIntegration.signApk(
-    input = File("app/build/outputs/apk/release/app-release-unsigned.apk"),
-    output = File("app/build/outputs/apk/release/app-release.apk")
+        input = File("app/build/outputs/apk/release/app-release-unsigned.apk"),
+        output = File("app/build/outputs/apk/release/app-release.apk")
 )
 
 if (GithubActions.isTag()) {
     println("creating release on github...")
 
     val assets = listOf(
-        zip("kotlin-hslog/build/bin/macosX64/debugFramework/kotlin_hslog.framework", "kotlin_hslog.framework.debug.zip"),
-        zip("kotlin-hslog/build/bin/macosX64/debugFramework/kotlin_hslog.framework.dSYM", "kotlin_hslog.framework.dSYM.debug.zip"),
-        zip("kotlin-hslog/build/bin/macosX64/releaseFramework/kotlin_hslog.framework", "kotlin_hslog.framework.release.zip"),
-        File("app/build/outputs/apk/debug/app-debug.apk"),
-        File("app/build/outputs/apk/release/app-release.apk")
+            zip("kotlin-hslog/build/bin/macosX64/debugFramework/kotlin_hslog.framework", "kotlin_hslog.framework.debug.zip"),
+            zip("kotlin-hslog/build/bin/macosX64/debugFramework/kotlin_hslog.framework.dSYM", "kotlin_hslog.framework.dSYM.debug.zip"),
+            zip("kotlin-hslog/build/bin/macosX64/releaseFramework/kotlin_hslog.framework", "kotlin_hslog.framework.release.zip"),
+            zip("kotlin-hslog/build/bin/macosX64/releaseFramework/kotlin_hslog.framework.dSYM", "kotlin_hslog.framework.dSYM.release.zip"),
+            File("app/build/outputs/apk/debug/app-debug.apk"),
+            File("app/build/outputs/apk/release/app-release.apk")
     )
 
     GithubIntegration.createRelease(
-        token = KintaEnv.get("KINTA_GITHUB_TOKEN"),
-        tagName = GithubActions.tagName(),
-        assets = assets,
-        repo = "Arcane-Tracker",
-        owner = "Hearthsim",
-        changelogMarkdown = ""
+            token = KintaEnv.get("KINTA_GITHUB_TOKEN"),
+            tagName = GithubActions.tagName(),
+            assets = assets,
+            repo = "Arcane-Tracker",
+            owner = "Hearthsim",
+            changelogMarkdown = ""
     )
 }
