36b0459e5944e6e7c5b2ca5d2cf86ae07ea3d5f7
Martin Bonnin <martin@mbonnin.net>
2020-06-10 20:47:41 +0200

    do not run hsreplay api tests on CI

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -7,7 +7,7 @@ sdkmanager --install 'ndk;21.0.6113669'  >/dev/null
 
 echo "$GOOGLE_SERVICES_JSON" > app/google-services.json
 
-./gradlew :app:assembleDebug :app:assembleRelease jvmTest linkDebugFrameworkMacosX64 linkReleaseFrameworkMacosX64 -Dorg.gradle.jvmargs=-Xmx2g
+./gradlew :app:assembleDebug :app:assembleRelease jvmTest -x kotlin-hsreplay-api:jvmTest linkDebugFrameworkMacosX64 linkReleaseFrameworkMacosX64 -Dorg.gradle.jvmargs=-Xmx2g
 
 curl -s "https://get.sdkman.io" | bash > /dev/null
 source "$HOME/.sdkman/bin/sdkman-init.sh"
