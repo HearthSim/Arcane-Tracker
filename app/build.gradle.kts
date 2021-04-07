1cf5eb9c1c7d57890412ada929c948be5a95a18b
Martin Bonnin <martin@mbonnin.net>
2021-04-07 21:54:12 +0200

    version 461

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -110,8 +110,8 @@ android {
         applicationId = "net.mbonnin.arcanetracker"
         minSdkVersion(Versions.minSdkVersion)
         targetSdkVersion(Versions.targetSdkVersion)
-        versionCode = 460
-        versionName = "4.60"
+        versionCode = 461
+        versionName = "4.61"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         multiDexEnabled = true
